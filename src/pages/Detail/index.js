464c1b886fd12a5d3d3e7756622fbc79bc147221
Manogel <manoelgomes53@gmail.com>
2020-03-28 00:00:45 -0300

    page detail

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container } from './styles';
+import Header from '~/components/Header';
+import { Background } from '~/styles';
 
-export default function Detail() {
-  return <Container />;
+import {
+  Container,
+  BackgroundImage,
+  MainHeader,
+  Button,
+  ButtonName,
+  HtmlView,
+} from './styles';
+
+export default function Detail({ navigation: { getParam, goBack } }) {
+  const [page, setPage] = useState(1);
+
+  const { id, name } = getParam('topic');
+
+  const htmlContent = `<ul><li><strong>Sintomas</strong></li></ul>
+<p>As Infec&ccedil;&otilde;es Sexualmente Transmiss&iacute;veis (IST) s&atilde;o causadas por v&iacute;rus, bact&eacute;rias ou outros &nbsp;microrganismos. Elas s&atilde;o transmitidas.As Infec&ccedil;&otilde;es Sexualmente Transmiss&iacute;veis (IST) s&atilde;o causadas por v&iacute;rus, bact&eacute;rias ou outros &nbsp;microrganismos. Elas s&atilde;o transmitidas.<strong><br></strong></p><ul><li><strong><strong>Sobre</strong></strong></li>
+</ul>
+
+<p>As Infec&ccedil;&otilde;es Sexualmente Transmiss&iacute;veis (IST) s&atilde;o causadas por v&iacute;rus, bact&eacute;rias ou outros &nbsp;microrganismos. Elas s&atilde;o transmitidas.As Infec&ccedil;&otilde;es Sexualmente Transmiss&iacute;veis (IST) s&atilde;o causadas por v&iacute;rus, bact&eacute;rias ou outros &nbsp;microrganismos. Elas s&atilde;o transmitidas.As Infec&ccedil;&otilde;es Sexualmente Transmiss&iacute;veis (IST) s&atilde;o causadas por v&iacute;rus, bact&eacute;rias ou outros &nbsp;microrganismos. Elas s&atilde;o transmitidas.<strong><strong><br></strong></strong></p>
+
+<p>
+	<br>
+</p>
+
+`;
+  return (
+    <>
+      <Header title={name} goBack={goBack} />
+      <Background>
+        <MainHeader>
+          <Button active={page === 1} onPress={() => setPage(1)}>
+            <ButtonName active={page === 1}>Informações</ButtonName>
+          </Button>
+          <Button active={page === 2} onPress={() => setPage(2)}>
+            <ButtonName active={page === 2}>Imagens</ButtonName>
+          </Button>
+        </MainHeader>
+        <BackgroundImage />
+        <Container>
+          <HtmlView html={htmlContent} />
+        </Container>
+      </Background>
+    </>
+  );
 }
--- src/pages/Detail/styles.js
+++ src/pages/Detail/styles.js
@@ -1,3 +1,84 @@
-import styled from 'styled-components/native';
+import Html from 'react-native-render-html';
 
-export const Container = styled.View``;
+import styled, { css } from 'styled-components/native';
+
+import background2 from '~/assets/backgrounds/background2.png';
+import { colors, NunitoBold, PlatformType, metrics } from '~/styles';
+
+export const BackgroundImage = styled.Image.attrs({
+  source: background2,
+})`
+  position: absolute;
+  z-index: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+export const Container = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    marginHorizontal: 30,
+    paddingBottom: 20,
+    flex: 1,
+  },
+  showsVerticalScrollIndicator: false,
+})`
+  flex: 1;
+`;
+
+export const MainHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin: 20px 30px;
+`;
+
+export const Button = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  padding: 4px 0;
+
+  border-bottom-width: 1px;
+  border-bottom-color: ${colors.title};
+
+  ${({ active }) =>
+    active &&
+    css`
+      border-bottom-width: 3px;
+      border-bottom-color: ${colors.male};
+    `}
+`;
+
+export const ButtonName = styled(NunitoBold)`
+  font-size: 16px;
+  text-align: center;
+  color: ${colors.title}
+    ${({ active }) =>
+      active &&
+      css`
+        color: ${colors.male};
+      `};
+`;
+
+export const HtmlView = styled(Html).attrs({
+  imagesMaxWidth: metrics.screenWhidth - 60,
+  tagsStyles: {
+    p: {
+      textAlign: 'justify',
+      color: colors.subtitle,
+      lineHeight: 18,
+      fontSize: 14,
+    },
+    li: {
+      fontSize: 16,
+      color: colors.title,
+    },
+    ul: {
+      color: colors.title,
+    },
+    strong: {
+      color: colors.title,
+    },
+  },
+})``;
