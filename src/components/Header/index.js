37e8aa6dcdce49892e778e1a673d817b06b86674
Manogel <manoelgomes53@gmail.com>
2020-03-27 23:59:06 -0300

    Header config

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,110 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Animated } from 'react-native';
+import FAIcon from 'react-native-vector-icons/FontAwesome5';
 import FTIcon from 'react-native-vector-icons/Fontisto';
+import { withNavigation } from 'react-navigation';
 
-import { Container, Title, ButtonLeft, UnisexIcon } from './styles';
+import {
+  Container,
+  Gradient,
+  Content,
+  Title,
+  ButtonLeft,
+  UnisexIcon,
+  Action,
+  LabelAction,
+} from './styles';
 
-export default function Header() {
+function Header({ title, goBack, navigation: { navigate } }) {
+  const [isActive, setActive] = useState(false);
+  const [heightProgress] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    if (isActive) {
+      Animated.timing(heightProgress, {
+        toValue: 100,
+        duration: 300,
+      }).start();
+    } else {
+      Animated.timing(heightProgress, {
+        toValue: 0,
+        duration: 300,
+      }).start();
+    }
+  }, [isActive]);
+
+  const functionLeft = goBack !== false ? goBack : () => setActive(!isActive);
+
+  function renderButtonLeft() {
+    if (!goBack && !isActive) {
+      return <FTIcon color="#fff" size={24} name="nav-icon-grid-a" />;
+    }
+    if (goBack) {
+      return <FTIcon color="#fff" size={20} name="arrow-left" />;
+    }
+    return <FTIcon color="#fff" size={24} name="minus-a" />;
+  }
+
   return (
-    <Container>
-      <ButtonLeft>
-        <FTIcon color="#fff" size={24} name="nav-icon-grid-a" />
-      </ButtonLeft>
-      <Title>Conhecendo as IST`s</Title>
-      <UnisexIcon />
+    <Container
+      isActive={isActive}
+      style={{
+        height: heightProgress.interpolate({
+          inputRange: [0, 100],
+          outputRange: [110, 190],
+          extrapolate: 'clamp',
+        }),
+      }}
+    >
+      <Gradient>
+        <Content>
+          <ButtonLeft onPress={() => functionLeft()}>
+            {renderButtonLeft()}
+          </ButtonLeft>
+          <Title>{title}</Title>
+          <UnisexIcon />
+        </Content>
+
+        <Content
+          style={{
+            marginHorizontal: 40,
+            borderTopColor: '#EFEFEF',
+            borderTopWidth: 0.25,
+            height: heightProgress.interpolate({
+              inputRange: [0, 100],
+              outputRange: [0, 80],
+              extrapolate: 'clamp',
+            }),
+            opacity: heightProgress.interpolate({
+              inputRange: [0, 100],
+              outputRange: [0, 1],
+              extrapolate: 'clamp',
+            }),
+          }}
+        >
+          <Action onPress={() => navigate('ResultsSearch')}>
+            <FAIcon color="#EFEFEF" size={18} name="search" />
+            <LabelAction>Buscar</LabelAction>
+          </Action>
+
+          <Action>
+            <FAIcon color="#EFEFEF" size={18} name="language" />
+            <LabelAction>Idioma</LabelAction>
+          </Action>
+
+          <Action onPress={() => navigate('About')}>
+            <FAIcon color="#EFEFEF" size={18} name="info" />
+            <LabelAction>Sobre o APP</LabelAction>
+          </Action>
+        </Content>
+      </Gradient>
     </Container>
   );
 }
+
+Header.defaultProps = {
+  title: 'Conhecendo as IST`s',
+  goBack: false,
+};
+
+export default withNavigation(Header);
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,22 +1,42 @@
+import { Animated } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import LinearGradient from 'react-native-linear-gradient';
 
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 import unisex from '~/assets/icons/unisex.png';
-import { colors, NunitoBold, NunitoSemiBold } from '~/styles';
+import { colors, NunitoBold, Nunito } from '~/styles';
 
-export const Container = styled(LinearGradient).attrs({
+export const Container = styled(Animated.View)`
+  shadow-color: #000;
+  shadow-offset: 0 2px;
+  shadow-opacity: 0.25;
+  shadow-radius: 3.84px;
+  z-index: 5;
+  elevation: 5;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      border-bottom-left-radius: 1px;
+      border-bottom-right-radius: 1px;
+    `}
+`;
+
+export const Gradient = styled(LinearGradient).attrs({
   colors: [colors.primary, '#9BA2DA', colors.secundary],
   start: { x: 0, y: 0 },
   end: { x: 1, y: 0 },
 })`
-  flex-direction: row;
-  padding-top: ${getStatusBarHeight()}px;
-  height: 80px;
+  padding-top: ${getStatusBarHeight() + 10}px;
+`;
+
+export const Content = styled(Animated.View)`
   justify-content: space-between;
   align-items: center;
   text-align: center;
+  flex-direction: row;
+  height: 70px;
 `;
 
 export const Title = styled(NunitoBold)`
@@ -38,4 +58,16 @@ export const UnisexIcon = styled.Image.attrs({
   height: 40px;
   width: 40px;
   margin: 0 15px 0 10px;
+`;
+
+export const Action = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  align-items: center;
+`;
+
+export const LabelAction = styled(Nunito)`
+  color: #fff;
+  margin-top: 4px;
+  font-size: 12px;
 `;
