1fe4e7f28ed5a02457ef65f0ecce36d3c515c542
Manogel <manoelgomes53@gmail.com>
2020-03-28 00:00:37 -0300

    page about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,65 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Header from '~/components/Header';
+import { Background } from '~/styles';
 
-export default function About() {
-  return <Container />;
+import {
+  Container,
+  BackgroundImage,
+  TitleSession,
+  Card,
+  BorderGradient,
+  Info,
+  Title,
+  Introduction,
+  Bottom,
+} from './styles';
+
+export default function About({ navigation: { goBack } }) {
+  return (
+    <>
+      <Header title="Sobre o APP" goBack={goBack} />
+      <Background>
+        <BackgroundImage />
+        <Container>
+          <TitleSession>Projeto</TitleSession>
+          <Card>
+            <BorderGradient sex={0} />
+            <Info>
+              <Title>Ciely Ramos</Title>
+              <Introduction>
+                Graduanda do curso de ciências biológicas na universidade
+                federal do Ámapa!
+              </Introduction>
+            </Info>
+          </Card>
+          <TitleSession>Aplicação</TitleSession>
+          <Card>
+            <BorderGradient sex={1} />
+            <Info>
+              <Title>Manoel Gomes</Title>
+              <Introduction>
+                Graduando do curso de ciência da computação na universidade
+                federal do Ámapa!
+              </Introduction>
+            </Info>
+          </Card>
+          <TitleSession>Referências</TitleSession>
+          <Card style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+            <BorderGradient />
+            {[1, 2, 3, 4].map(i => (
+              <Info key={i}>
+                <Title>Titulo referencia</Title>
+                <Introduction>Referencia por extenso</Introduction>
+              </Info>
+            ))}
+          </Card>
+          <Bottom>
+            <Title>Conhecendo as IST's</Title>
+            <Introduction>V 1.0.0</Introduction>
+          </Bottom>
+        </Container>
+      </Background>
+    </>
+  );
 }
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,3 +1,88 @@
+import LinearGradient from 'react-native-linear-gradient';
+
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import background1 from '~/assets/backgrounds/background4.png';
+import { colors, NunitoBold, Nunito } from '~/styles';
+
+export const BackgroundImage = styled.Image.attrs({
+  source: background1,
+})`
+  position: absolute;
+  z-index: 0;
+  top: 10px;
+  bottom: 0;
+  left: 0;
+`;
+
+export const Container = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    paddingBottom: 20,
+    paddingHorizontal: 30,
+  },
+  showsVerticalScrollIndicator: false,
+})`
+  flex: 1;
+`;
+
+export const IdentCards = styled.View`
+  background: transparent;
+  margin-left: 10%;
+`;
+
+export const TitleSession = styled(NunitoBold)`
+  font-size: 18px;
+  margin: 25px 0;
+`;
+
+export const BorderGradient = styled(LinearGradient).attrs(({ sex }) => {
+  let color = [colors.female, colors.male];
+
+  if (sex === 0) {
+    color = [colors.female, colors.female];
+  }
+
+  if (sex === 1) {
+    color = [colors.male, colors.male];
+  }
+
+  return {
+    colors: color,
+    start: { x: 0, y: 0 },
+    end: { x: 1, y: 1 },
+  };
+})`
+  position: absolute;
+  left: 0;
+  width: 5px;
+  height: 100%;
+  border-bottom-left-radius: 2px;
+  border-top-left-radius: 2px;
+`;
+
+export const Card = styled.View`
+  flex-direction: row;
+  align-items: center;
+  background: #fff;
+  padding-right: 10px;
+  border-radius: 2px;
+`;
+
+export const Info = styled.View`
+  padding: 10px 10px 10px 15px;
+  flex: 1;
+`;
+
+export const Title = styled(NunitoBold)`
+  font-size: 16px;
+`;
+
+export const Introduction = styled(Nunito)`
+  margin: 10px 0;
+`;
+
+export const Bottom = styled.View`
+  margin: 30px 0 20px;
+  flex-direction: column;
+  align-items: center;
+`;
